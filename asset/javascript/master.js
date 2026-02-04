63ae91d8d545e9e6f38857af10e31d89fec90b20
Ocean <ocean@example.com>
2026-02-04 13:48:07 +0000

    website template

---
+++ asset/javascript/master.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const toggle = document.getElementById('toggle');
+    const menu = document.getElementById('menu');
+
+    toggle.addEventListener('click' , function() {
+        menu.classList.toggle('open');
+    });
+});
+
+//First website template  @Drench
